c98c1c44a61c12bf2dc4fac5423090b3342fe79e
joyce-r <50540465+joyce-r@users.noreply.github.com>
2020-04-18 04:40:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#git clone https://github.com/fw876/helloworld                package/luci-app-ssr-plus
 
 #添加其他软件包
 #git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-Jo
