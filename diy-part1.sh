1793673fdbd967865c306c26ecc90b72c56c1583
Ed <50540465+Ed-11@users.noreply.github.com>
2022-07-23 12:36:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git passwall https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 
 #添加其他软件包
